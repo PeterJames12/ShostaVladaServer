57e4bed733979c9ed66c244495a9c823a2e0c428
Igor <joyukr>
2017-06-20 00:58:52 +0300

    added plugin war in pom.xml

---
+++ src/main/java/shosta/vlada/server/config/DatabaseConfig.java
@@ -0,0 +1,12 @@
+package shosta.vlada.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Igor Hnes on 6/20/17.
+ */
+@Configuration
+
+public class DatabaseConfig {
+
+}
